25de30ce280bb00c42a1b88f100a585661c771ed
ChitraBala <chitrabala1989@gmail.com>
2024-09-03 13:27:57 +0530

    folder name changed

--- streamlit.py
+++ streamlit.py
@@ -3,18 +3,18 @@ import streamlit as st
 
 # --- PAGE SETUP ---
 about_page =st.Page(
-    "C:/Users/Elcot/PycharmProject/pythonProject/web application/deploy/about_me.py",
+    "C:/Users/Elcot/PycharmProject/pythonProject/Web-Application/deploy/about_me.py",
     title="About Me",
     icon=":material/account_circle:",
     default=True,
 )
 project_1_page = st.Page(
-    "C:/Users/Elcot/PycharmProject/pythonProject/web application/deploy/sales_dashboard.py",
+    "C:/Users/Elcot/PycharmProject/pythonProject/Web-Application/deploy/sales_dashboard.py",
     title="Sales Dashboard",
     icon=":material/bar_chart:",
 )
 project_2_page = st.Page(
-    "C:/Users/Elcot/PycharmProject/pythonProject/web application/deploy/chatbot.py",
+    "C:/Users/Elcot/PycharmProject/pythonProject/Web-Application/deploy/chatbot.py",
     title="Chat Bot",
     icon=":material/smart_toy:",
 )
@@ -32,7 +32,7 @@ pg = st.navigation(
 )
 
 # --- SHARED ON ALL PAGES ---
-st.logo("C:/Users/Elcot/PycharmProject/pythonProject/web application/assets/logo1.png")
+st.logo("C:/Users/Elcot/PycharmProject/pythonProject/Web-Application/assets/logo1.png")
 st.sidebar.markdown("Made with 😊 by [Chitra](https://youtube.com/@codingisfun)")
 
 pg.run()
