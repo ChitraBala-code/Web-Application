02ec89911d06cf1c56a35a3130277431baf59d87
ChitraBala <chitrabala1989@gmail.com>
2024-09-03 12:46:21 +0530

    py file

---
+++ streamlit.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+
+# --- PAGE SETUP ---
+about_page =st.Page(
+    "C:/Users/Elcot/PycharmProject/pythonProject/web application/deploy/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    "C:/Users/Elcot/PycharmProject/pythonProject/web application/deploy/sales_dashboard.py",
+    title="Sales Dashboard",
+    icon=":material/bar_chart:",
+)
+project_2_page = st.Page(
+    "C:/Users/Elcot/PycharmProject/pythonProject/web application/deploy/chatbot.py",
+    title="Chat Bot",
+    icon=":material/smart_toy:",
+)
+
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+#pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page, project_2_page],
+    }
+)
+
+# --- SHARED ON ALL PAGES ---
+st.logo("C:/Users/Elcot/PycharmProject/pythonProject/web application/assets/logo1.png")
+st.sidebar.markdown("Made with 😊 by [Chitra](https://youtube.com/@codingisfun)")
+
+pg.run()
