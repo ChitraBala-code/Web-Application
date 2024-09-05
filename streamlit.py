3927ad93560d500e231dd8b48047183cf3bdc604
ChitraBala-code <chitrabala1989@gmail.com>
2024-09-05 18:59:45 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Data manager", page_icon=":material/edit:", layou
                    )
 # --- PAGE SETUP ---
 about_page =st.Page(
-    "https://github.com/ChitraBala-code/Web-Application/blob/main/deploy/about_me.py",
+    "deploy/about_me.py",
     title="About Me",
     icon=":material/account_circle:",
     default=True,
